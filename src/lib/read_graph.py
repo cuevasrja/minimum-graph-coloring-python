8e4d59f3a78c04dd6f9a3beea96571a5171a4495
Juan Cuevas <juanandrescuevas14@gmail.com>
2024-05-14 20:33:29 -0400

    chore: Update .gitignore file to ignore additional files and directories. Optimize read_graph

--- src/lib/read_graph.py
+++ src/lib/read_graph.py
@@ -3,16 +3,19 @@ import igraph as ig
 def read_graph(file_path: str) -> ig.Graph:
     with open(file_path, "r") as f:
         lines = f.readlines()
+    i: int = 0
     lines: list[str] = [line.strip() for line in lines]
     format: str = ''
     nodes: int = 0
     edges: int = 0
     for line in lines:
         if line.startswith("c"):
+            i += 1
             continue
         elif line.startswith("p"):
             format, nodes, edges = line.split(' ')[1:]
             nodes, edges = int(nodes), int(edges)
+            i += 1
             break
     g = ig.Graph(directed=False)
 
@@ -20,9 +23,9 @@ def read_graph(file_path: str) -> ig.Graph:
         g.add_vertex(name=str(i+1), color="#")
 
     if format == "edge":
-        for line in lines:
-            if line.startswith("e"):
-                _, v1, v2 = line.split(' ')
+        for j in range(i, len(lines)):
+            if lines[j].startswith("e"):
+                _, v1, v2 = lines[j].split(' ')
                 g.add_edge(v1, v2)
     else:
         print("\033[91mError: The graph format is not supported.\033[0m")
