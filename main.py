5e44c5d914372a18193b1d62208a0716442f3344
joaobose <joaobose@gmail.com>
2024-05-19 18:17:27 -0400

    mejora del informe

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from src.lib.read_graph import read_graph
 import time
 import threading
 
+
 def run_with_timeout(func, timeout):
-    """Ejecuta una función con un límite de tiempo y devuelve True si se completó a tiempo, False si se excedió el tiempo."""
+    """
+    Ejecuta una función con un límite de tiempo y devuelve True si se completó a tiempo, False si se excedió el tiempo.
+    """
+
     def wrapper(stop_event):
         try:
             func()
@@ -22,64 +26,79 @@ def run_with_timeout(func, timeout):
         print("El tiempo de ejecución excede los 5 minutos, saltando esta parte del código")
         stop_event.set()  # Señalar que el hilo debe detenerse
         thread.join()  # Esperar a que el hilo termine
+
         return False
+
     return True
 
+
 def main():
-    # Leemos los argumentos de la línea de comandos
+    # Se leen los argumentos de la línea de comandos
     file_path: str = sys.argv[1]
     g: ig.Graph = read_graph(file_path)
-    # print(g)
-    # Muestro la cantidad de lados
-    print("\033[103;1mCantidad de lados:\033[0m \033[93m", len(g.es), "\033[0m")
-    # Muestro la cantidad de nodos
-    print("\033[103;1mCantidad de nodos:\033[0m \033[93m", len(g.vs), "\033[0m")
+
+    # Se muestran la cantidad de lados
+    print("\033[103;1mCantidad de lados:\033[0m \033[93m",
+          len(g.es), "\033[0m")
+    # Se muestran la cantidad de nodos
+    print("\033[103;1mCantidad de nodos:\033[0m \033[93m",
+          len(g.vs), "\033[0m")
 
     # Invocando D-Satur
     print("\n\033[100;1mInvocando D-Satur...\033[0m")
 
     g.reset_colors()
     start_time = time.time()
-    
+
     if run_with_timeout(g.d_satur, 300):
         end_time = time.time()
         execution_time = end_time - start_time
         print(f"Tiempo de ejecución: {execution_time} segundos")
+
         g.group_nodes_by_color()
-        is_valid: str = "\033[92;1mTrue" if g.is_valid_coloring() else "\033[91mFalse"
+
+        is_valid: str = "\033[92;1mTrue" if g.is_valid_coloring(
+        ) else "\033[91mFalse"
         print(f"Coloración válida: {is_valid}\033[0m")
 
     g.reset_colors()
-    
+
     # Invocando Local Search
-    print("\n\033[100;1mInvocando Local Search...\033[0m")       
+    print("\n\033[100;1mInvocando Local Search...\033[0m")
     start_time = time.time()
-    
+
     if run_with_timeout(g.local_search, 300):
         end_time = time.time()
         execution_time = end_time - start_time
         print(f"Tiempo de ejecución: {execution_time} segundos")
+
         g.group_nodes_by_color()
-        is_valid: str = "\033[92;1mTrue" if g.is_valid_coloring() else "\033[91mFalse"
-        print(f"Coloración válida: {is_valid}\033[0m")         
+
+        is_valid: str = "\033[92;1mTrue" if g.is_valid_coloring(
+        ) else "\033[91mFalse"
+        print(f"Coloración válida: {is_valid}\033[0m")
 
     # Invocando Backtracking
     print("\n\033[100;1mInvocando Backtracking...\033[0m")
 
     g.reset_colors()
-    
+
     if len(g.vs) == 0:
         print("\033[91;1mError:\033[0m Backtracking no pudo colorear el grafo")
-    else: 
+    else:
         start_time = time.time()
-    
+
         if run_with_timeout(g.backtracking, 120):
             end_time = time.time()
             execution_time = end_time - start_time
             print(f"Tiempo de ejecución: {execution_time} segundos")
+
             g.group_nodes_by_color()
-            is_valid: str = "\033[92;1mTrue" if g.is_valid_coloring() else "\033[91mFalse"
-            print(f"Coloración válida: {is_valid}\033[0m")        
+
+            is_valid: str = "\033[92;1mTrue" if g.is_valid_coloring(
+            ) else "\033[91mFalse"
+            print(f"Coloración válida: {is_valid}\033[0m")
+
 
 if __name__ == "__main__":
     main()
