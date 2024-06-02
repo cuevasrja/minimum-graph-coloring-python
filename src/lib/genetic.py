89e4d02edb6aeb76f6cf6d88b6f051f1a642c267
Juan Cuevas <juanandrescuevas14@gmail.com>
2024-06-02 14:05:07 -0400

    feat: Agregar tipado

--- src/lib/genetic.py
+++ src/lib/genetic.py
@@ -1,19 +1,20 @@
+from typing import List
 import igraph as ig
 import random
 
 def genetic_algorithm(self: ig.Graph, population_size: int = 100, generations: int = 100, mutation_rate: float = 0.01):
     # Generar población inicial
-    population = [random_coloring(self) for _ in range(population_size)]
+    population: List[int] = [random_coloring(self) for _ in range(population_size)]
     
     for _ in range(generations):
         # Evaluar aptitud
-        fitness = [color_count(coloring) for coloring in population]
+        fitness: List[int] = [color_count(coloring) for coloring in population]
         
         # Seleccionar padres
-        parents = random.choices(population, weights=fitness, k=2)
+        parents: List[int] = random.choices(population, weights=fitness, k=2)
         
         # Cruzar padres para crear hijo
-        child = crossover(parents[0], parents[1])
+        child: List[int] = crossover(parents[0], parents[1])
         
         # Mutar hijo
         if random.random() < mutation_rate:
@@ -23,7 +24,7 @@ def genetic_algorithm(self: ig.Graph, population_size: int = 100, generations: i
         population.append(child)
         
         # Eliminar individuo menos apto
-        least_fit_index = fitness.index(min(fitness))
+        least_fit_index: int = fitness.index(min(fitness))
         population.pop(least_fit_index)
     
     # Devolver individuo más apto
@@ -31,19 +32,19 @@ def genetic_algorithm(self: ig.Graph, population_size: int = 100, generations: i
     for i, color in enumerate(best_coloring):
         self.vs[i]["color"] = color
 
-def random_coloring(graph: ig.Graph):
+def random_coloring(graph: ig.Graph) -> List[int]:
     # Asignar a cada nodo un color aleatorio
     return [random.randint(0, len(graph.vs) - 1) for _ in graph.vs]
 
-def color_count(coloring):
+def color_count(coloring: List[int]) -> int:
     # Contar el número de colores utilizados
     return len(set(coloring))
 
-def crossover(parent1, parent2):
+def crossover(parent1: int, parent2: int) -> List[int]:
     # Crear un nuevo individuo combinando los colores de los padres
     return [parent1[i] if random.random() < 0.5 else parent2[i] for i in range(len(parent1))]
 
-def mutate(coloring):
+def mutate(coloring: List[int]) -> None:
     # Cambiar el color de un nodo aleatorio
     coloring[random.randint(0, len(coloring) - 1)] = random.randint(0, len(coloring) - 1)
     
