b07c61b1d4affab959915436de74072349c12d35
joaobose <joaobose@gmail.com>
2024-06-03 21:41:59 -0400

    ILS enhancements and seed lock

--- src/lib/ils.py
+++ src/lib/ils.py
@@ -4,19 +4,18 @@ import math
 import random
 
 
-def calculate_rounded_percentages(number: int) -> List[int]:
+def calculate_perturbation_amounts(n_colors: int) -> List[int]:
     """
-    Calcula los porcentajes de un número en incrementos de 0.05 desde 0.05 hasta 0.50 y redondea hacia arriba.
+    Calcula la cantidad de colores a eliminar en cada perturbación para la implementación de ILS.
     """
-    percentages = []
+    values = []
 
-    # Iterar sobre los valores de porcentaje en incrementos de 0.05 desde 0.05 hasta 0.65
     for percent in range(5, 65, 5):
-        # Calcular el porcentaje correspondiente y redondear hacia arriba
-        percentage_value = math.ceil(number * percent / 100)
-        percentages.append(percentage_value)
+        values.append(math.ceil(n_colors * percent / 100))
 
-    return percentages
+    min_value = min(values)
+
+    return [n for n in range(1, 4) if n < min_value] + values
 
 
 def get_random_values(array, n):
@@ -35,19 +34,21 @@ def ils(self: ig.Graph):
 
     amount_of_colors = self.number_of_colors()
     current_colors_in_graph = self.colors_used()
-    iterations_percentage = calculate_rounded_percentages(amount_of_colors)
     best_solution = self.coloring_as_dict()
     best_number_of_colors = amount_of_colors
 
-    NON_IMPROVEMENT_LIMIT = 5
+    # Calcular la cantidad de colores a eliminar en cada perturbación
+    perturbation_amounts = calculate_perturbation_amounts(amount_of_colors)
+
+    NON_IMPROVEMENT_LIMIT = 6
     non_improvement_count = 0
+    random.seed(42)
 
     # Por cada ronda de perturbación
-    for clear_pct in iterations_percentage:
-        # Calcular cuantos colores se deben eliminar
-        expeted_to_clear = int(clear_pct * amount_of_colors / 100)
+    for expected_to_clear in perturbation_amounts:
+        # Obtener los colores que se deben eliminar
         colors_to_clear = get_random_values(
-            current_colors_in_graph, expeted_to_clear)
+            current_colors_in_graph, expected_to_clear)
 
         # Perturbar la solución eliminando colores
         for color in colors_to_clear:
